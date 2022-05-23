be6ee62eaf0d4e90280930dd6566ffb6e4ea379c
cw_51 <cw_51@ASUS-BEAST>
2022-05-23 14:58:10 +0530

    Commit on 20-05-2022

--- corejava/src/corejava_package/FirstProgram.java
+++ corejava/src/corejava_package/FirstProgram.java
@@ -1,10 +1,29 @@
 package corejava_package;
+import java.util.*;
 
 public class FirstProgram {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// TODO Print Whether given number is prime or not.
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the num value:");
+		int num = sc.nextInt();
+		
+		int c=2;
+		while(c*c<num)
+		{
+			if(num%c==0)
+			{
+				System.out.println("Number is not prime.");
+				System.out.println("Thankyou");
+				System.exit(0);
+			}
+			c=c+1;
+		}
+		System.out.println("Number is prime.");
+		{
+			System.out.println("Thankyou");
+		}
 	}
 
 }
